4ef1b8aa6b40fee0ada919498e02b21551dc4e7c
Landon Reed <landontreed@gmail.com>
2016-05-10 17:19:01 -0400

    added class name to generic gtfserror

--- src/main/java/com/conveyal/gtfs/error/DuplicateStopError.java
+++ src/main/java/com/conveyal/gtfs/error/DuplicateStopError.java
@@ -7,12 +7,10 @@ public class DuplicateStopError extends GTFSError {
 
     private String message;
     public DuplicateStops duplicateStop;
-    public String errorType;
     public DuplicateStopError(String message, DuplicateStops duplicateStop) {
         super("stop", 0, "stop_lat,stop_lon");
         this.message = message;
         this.duplicateStop = duplicateStop;
-        this.errorType = this.getClass().toString();
     }
 
     @Override public String getMessage() {
