2bdd01f37718ef4b4096b6fc5a7cfb5971572d4d
Matthew Wigginton Conway <matt@indicatrix.org>
2016-06-14 16:18:13 -0400

    Code to have LRU cache of GTFS feeds from S3.

---
+++ src/main/java/com/conveyal/gtfs/GTFSCache.java
@@ -0,0 +1,158 @@
+package com.conveyal.gtfs;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.io.ByteStreams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+import java.util.zip.ZipFile;
+
+/**
+ * Fast cache for GTFS feeds stored on S3.
+ */
+public class GTFSCache {
+    private static final Logger LOG = LoggerFactory.getLogger(GTFSCache.class);
+
+    public final String bucket;
+
+    public final File cacheDir;
+
+    private static final AmazonS3 s3 = new AmazonS3Client();
+
+    private LoadingCache<String, GTFSFeed> cache = CacheBuilder.newBuilder()
+            .maximumSize(20)
+            .build(new CacheLoader<String, GTFSFeed>() {
+                @Override
+                public GTFSFeed load(String s) throws Exception {
+                    return retrieveFeed(s);
+                }
+            });
+
+    public GTFSCache(String bucket, File cacheDir) {
+        this.bucket = bucket;
+        this.cacheDir = cacheDir;
+    }
+
+    /**
+     * Add a GTFS feed to this cache with the given ID. NB this is not the feed ID, because feed IDs are not
+     * unique when you load multiple versions of the same feed.
+     */
+    public GTFSFeed put (String id, File feedFile) throws Exception {
+        return put(id, feedFile, null);
+    }
+
+    /** Add a GTFS feed to this cache where the ID is calculated from the feed itself */
+    public GTFSFeed put (Function<GTFSFeed, String> idGenerator, File feedFile) throws Exception {
+        return put(null, feedFile, idGenerator);
+    }
+
+    private GTFSFeed put (String id, File feedFile, Function<GTFSFeed, String> idGenerator) throws Exception {
+        // generate temporary ID to name files
+        String tempId = id != null ? id : UUID.randomUUID().toString();
+
+        // read the feed
+        String cleanTempId = cleanId(tempId);
+        File dbFile = new File(cacheDir, cleanTempId + ".db");
+        GTFSFeed feed = new GTFSFeed(dbFile.getAbsolutePath());
+        feed.loadFromFile(new ZipFile(feedFile));
+        feed.findPatterns();
+
+        if (idGenerator != null) id = idGenerator.apply(feed);
+
+        String cleanId = cleanId(id);
+
+        feed.close(); // make sure everything is written to disk
+
+        if (idGenerator != null) {
+            new File(cacheDir, cleanTempId + ".db").renameTo(new File(cacheDir, cleanId + ".db"));
+            new File(cacheDir, cleanTempId + ".db.p").renameTo(new File(cacheDir, cleanId + ".db.p"));
+        }
+
+        // upload feed
+        // TODO best way to do this? Should we zip the files together?
+        s3.putObject(bucket, cleanId + ".zip", feedFile);
+        s3.putObject(bucket, cleanId + ".db", new File(cacheDir, cleanId + ".db"));
+        s3.putObject(bucket, cleanId + ".db.p", new File(cacheDir, cleanId + ".db.p"));
+
+        // reconnect to feed database
+        feed = new GTFSFeed(new File(cacheDir, cleanId + ".db").getAbsolutePath());
+        cache.put(id, feed);
+        return feed;
+    }
+
+    public GTFSFeed get (String id) {
+        try {
+            return cache.get(id);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** retrieve a feed from local cache or S3 */
+    private GTFSFeed retrieveFeed (String originalId) {
+        // see if we have it cached locally
+        String id = cleanId(originalId);
+        File dbFile = new File(cacheDir, id + ".db");
+        if (dbFile.exists()) {
+            LOG.info("Processed GTFS was found cached locally");
+            return new GTFSFeed(dbFile.getAbsolutePath());
+        }
+
+        try {
+            LOG.info("Attempting to download cached GTFS MapDB.");
+            S3Object db = s3.getObject(bucket, id + ".db");
+            InputStream is = db.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(dbFile);
+            ByteStreams.copy(is, fos);
+            is.close();
+            fos.close();
+
+            S3Object dbp = s3.getObject(bucket, id + ".db.p");
+            InputStream isp = dbp.getObjectContent();
+            FileOutputStream fosp = new FileOutputStream(new File(cacheDir, id + ".db.p"));
+            ByteStreams.copy(isp, fosp);
+            isp.close();
+            fosp.close();
+
+            LOG.info("Returning processed GTFS from S3");
+            return new GTFSFeed(dbFile.getAbsolutePath());
+        } catch (AmazonServiceException | IOException e) {
+            LOG.info("Error retrieving MapDB from S3, will download original GTFS.", e);
+        }
+
+        // if we fell through to here, getting the mapdb was unsuccessful
+        // grab GTFS from S3.
+        try {
+            File feedFile = new File(cacheDir, id + ".zip");
+            S3Object gtfs = s3.getObject(bucket, id + ".zip");
+            InputStream is = gtfs.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(feedFile);
+            ByteStreams.copy(is, fos);
+            is.close();
+            fos.close();
+
+            // TODO this will also re-upload the original feed ZIP to S3.
+            return put(originalId, feedFile);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String cleanId(String id) {
+        return id.replaceAll("[^A-Za-z0-9]", "-");
+    }
+}
