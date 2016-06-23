99013f926450aaa3078d13f61fb8da81fd3488af
Landon Reed <landontreed@gmail.com>
2016-06-23 16:45:50 -0400

    made fare fields public

--- src/main/java/com/conveyal/gtfs/model/Fare.java
+++ src/main/java/com/conveyal/gtfs/model/Fare.java
@@ -14,9 +14,9 @@ import java.util.Map;
 public class Fare implements Serializable {
     public static final long serialVersionUID = 1L;
 
-    String         fare_id;
-    FareAttribute  fare_attribute;
-    List<FareRule> fare_rules = Lists.newArrayList();
+    public String         fare_id;
+    public FareAttribute  fare_attribute;
+    public List<FareRule> fare_rules = Lists.newArrayList();
 
     public Fare(String fare_id) {
         this.fare_id = fare_id;
