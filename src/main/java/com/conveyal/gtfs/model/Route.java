8e1174c5bc93b9ddcffec570b6ce33b887b88ca9
Landon Reed <landontreed@gmail.com>
2017-05-11 11:27:46 -0400

    rename operator -> agency

--- src/main/java/com/conveyal/gtfs/model/Route.java
+++ src/main/java/com/conveyal/gtfs/model/Route.java
@@ -1,7 +1,7 @@
 package com.conveyal.gtfs.model;
 
 import com.conveyal.gtfs.GTFSFeed;
-import com.conveyal.gtfs.error.NoOperatorInFeedError;
+import com.conveyal.gtfs.error.NoAgencyInFeedError;
 
 import java.io.IOException;
 import java.net.URL;
@@ -55,7 +55,7 @@ public class Route extends Entity { // implements Entity.Factory<Route>
                 if (feed.agency.size() == 1) {
                     r.agency_id = feed.agency.values().iterator().next().agency_id;
                 } else if (feed.agency.isEmpty()) {
-                    feed.errors.add(new NoOperatorInFeedError());
+                    feed.errors.add(new NoAgencyInFeedError());
                 }
             } else {
                 r.agency_id = agency.agency_id;
