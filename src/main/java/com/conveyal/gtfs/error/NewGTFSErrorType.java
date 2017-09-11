54a436d8be9653849ec4d6dc2f9631928d70223a
Andrew Byrd <andrew@fastmail.net>
2017-09-11 13:19:21 +0200

    Add strings descriptions to error enum

--- src/main/java/com/conveyal/gtfs/error/NewGTFSErrorType.java
+++ src/main/java/com/conveyal/gtfs/error/NewGTFSErrorType.java
@@ -2,61 +2,58 @@ package com.conveyal.gtfs.error;
 
 import com.conveyal.gtfs.validator.model.Priority;
 
-/**
- * TODO remove entity type prefixes where not needed
- */
 public enum NewGTFSErrorType {
 
-    DATE_FORMAT, // Format should be YYYYMMDD
-    TIME_FORMAT, // Format should be HH:MM:SS
-    URL_FORMAT, // Format should be <scheme>://<authority><path>?<query>#<fragment>
-    INTEGER_FORMAT,
-    FLOATING_FORMAT,
-    TABLE_NAME_FORMAT,
-    NUMBER_NEGATIVE,
-    NUMBER_RANGE, // Number %s outside of acceptable range
-    DUPLICATE_ID,
-    DUPLICATE_TRIP,
-    DUPLICATE_STOP,
-    DUPLICATE_HEADER,
-    MISSING_TABLE, // This table is required by the GTFS specification but is missing
-    MISSING_COLUMN,
-    MISSING_SHAPE,
-    MISSING_FIELD,
-    WRONG_NUMBER_OF_FIELDS,
-    OVERLAPPING_TRIP,
-    SHAPE_REVERSED,
-    SHAPE_MISSING_COORDINATE,
-    TABLE_IN_SUBDIRECTORY, // All GTFS files (including %s.txt) should be at root of zipfile, not nested in subdirectory
-    TABLE_TOO_LONG, // Table is too long to record line numbers with an integer, overflow will occur.
-    TIME_ZONE_FORMAT,
-    ROUTE_DESCRIPTION_SAME_AS_NAME,
-    ROUTE_LONG_NAME_CONTAINS_SHORT_NAME,
-    ROUTE_SHORT_AND_LONG_NAME_MISSING,
-    ROUTE_SHORT_NAME_TOO_LONG,
-    STOP_LOW_POPULATION_DENSITY,
-    STOP_GEOGRAPHIC_OUTLIER,
-    STOP_UNUSED,
-    TRIP_EMPTY,
-    ROUTE_UNUSED,
-    TRAVEL_DISTANCE_ZERO,
-    TRAVEL_TIME_NEGATIVE,
-    TRAVEL_TIME_ZERO,
-    MISSING_ARRIVAL_OR_DEPARTURE,
-    TRIP_TOO_FEW_STOP_TIMES,
-    TRIP_OVERLAP_IN_BLOCK,
-    TRAVEL_TOO_SLOW,
-    TRAVEL_TOO_FAST,
-    DEPARTURE_BEFORE_ARRIVAL,
-    OTHER;
+    DATE_FORMAT(Priority.MEDIUM, "Date format should be YYYYMMDD."),
+    TIME_FORMAT(Priority.MEDIUM, "Time format should be HH:MM:SS."),
+    URL_FORMAT(Priority.MEDIUM, "URL format should be <scheme>://<authority><path>?<query>#<fragment>"),
+    INTEGER_FORMAT(Priority.MEDIUM, "Incorrect integer format."),
+    FLOATING_FORMAT(Priority.MEDIUM, "Incorrect floating point number format."),
+    TABLE_NAME_FORMAT(Priority.MEDIUM, "Incorrect table name format."), // What is this?
+    NUMBER_NEGATIVE(Priority.MEDIUM, "Number was expected to be non-negative."),
+    NUMBER_RANGE(Priority.MEDIUM, "Number was outside of allowed range."),
+    DUPLICATE_ID(Priority.MEDIUM, "More than one entity in a table had the same ID."),
+    DUPLICATE_TRIP(Priority.MEDIUM, "More than one trip had an identical schedule and stops."),
+    DUPLICATE_STOP(Priority.MEDIUM, "More than one stop was located in exactly the same place."),
+    DUPLICATE_HEADER(Priority.MEDIUM, "More than one column in a table had the same name in the header row."),
+    MISSING_TABLE(Priority.MEDIUM, "This table is required by the GTFS specification but is missing."),
+    MISSING_COLUMN(Priority.MEDIUM, "A required column was missing from a table."),
+    MISSING_SHAPE(Priority.MEDIUM, "???"),
+    MISSING_FIELD(Priority.MEDIUM, "A required field was missing or empty in a particular row."),
+    WRONG_NUMBER_OF_FIELDS(Priority.MEDIUM, "A row did not have the same number of fields as there are headers in its table."),
+    OVERLAPPING_TRIP(Priority.MEDIUM, "Blocks?"),
+    SHAPE_REVERSED(Priority.MEDIUM, "A shape appears to be intended for vehicles running the opposite direction on the route."),
+    SHAPE_MISSING_COORDINATE(Priority.MEDIUM, "???"),
+    TABLE_IN_SUBDIRECTORY(Priority.HIGH, "Rather than being at the root of the zip file, a table was nested in a subdirectory."),
+    TABLE_TOO_LONG(Priority.MEDIUM, "Table is too long to record line numbers with a 32-bit integer, overflow will occur."),
+    TIME_ZONE_FORMAT(Priority.MEDIUM, "Time zone format should be X."),
+    ROUTE_DESCRIPTION_SAME_AS_NAME(Priority.LOW, "The description of a route is identical to its name, so does not add any information."),
+    ROUTE_LONG_NAME_CONTAINS_SHORT_NAME(Priority.LOW, "The long name of a route should complement the short name, not include it."),
+    ROUTE_SHORT_AND_LONG_NAME_MISSING(Priority.MEDIUM, "A route has neither a long nor a short name."),
+    ROUTE_SHORT_NAME_TOO_LONG(Priority.MEDIUM, "The short name of a route is too long for display in standard GTFS consumer applications."),
+    STOP_LOW_POPULATION_DENSITY(Priority.HIGH, "A stop is located in a geographic area with very low human population density."),
+    STOP_GEOGRAPHIC_OUTLIER(Priority.HIGH, "This stop is located very far from the middle 90% of stops in this feed."),
+    STOP_UNUSED(Priority.MEDIUM, "This stop is not referenced by any trips."),
+    TRIP_EMPTY(Priority.HIGH, "This trip is defined but has no stop times."),
+    ROUTE_UNUSED(Priority.HIGH, "This route is defined but has no trips."),
+    TRAVEL_DISTANCE_ZERO(Priority.MEDIUM, "The vehicle does not cover any distance between the last stop and this one."),
+    TRAVEL_TIME_NEGATIVE(Priority.HIGH, "The vehicle arrives at this stop before it departs from the previous one."),
+    TRAVEL_TIME_ZERO(Priority.HIGH, "The vehicle arrives at this stop at the same time it departs from the previous stop."),
+    MISSING_ARRIVAL_OR_DEPARTURE(Priority.MEDIUM, "First and last stop times are required to have both an arrival and departure time."),
+    TRIP_TOO_FEW_STOP_TIMES(Priority.MEDIUM, "A trip must have at least two stop times to represent travel."),
+    TRIP_OVERLAP_IN_BLOCK(Priority.MEDIUM, "Blocks"),
+    TRAVEL_TOO_SLOW(Priority.MEDIUM, "The vehicle is traveling very slowly to reach this stop from the previous one."),
+    TRAVEL_TOO_FAST(Priority.MEDIUM, "The vehicle travels extremely fast to reach this stop from the previous one."),
+    DEPARTURE_BEFORE_ARRIVAL(Priority.MEDIUM, "The vehicle departs from this stop before it arrives."),
+    REFERENTIAL_INTEGRITY(Priority.HIGH, "This line references an ID that does not exist in the target table."),
+    OTHER(Priority.LOW, "Other errors.");
 
     final Priority priority;
+    final String englishMessage;
 
-    NewGTFSErrorType() {
-        this.priority = Priority.MEDIUM;
-    }
-    NewGTFSErrorType(Priority priority) {
+    NewGTFSErrorType(Priority priority, String englishMessage) {
         this.priority = priority;
+        this.englishMessage = englishMessage;
     }
 
 }
