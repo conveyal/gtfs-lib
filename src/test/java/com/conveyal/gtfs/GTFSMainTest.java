8f4874649833dc7b4b3297f95a2d4dc4f9de8b2d
Evan Siroky <evan.siroky@yahoo.com>
2017-09-15 18:31:58 -0700

    Add first integration test of super small agency

--- src/test/java/com/conveyal/gtfs/GTFSMainTest.java
+++ src/test/java/com/conveyal/gtfs/GTFSMainTest.java
@@ -1,4 +1,45 @@
 package com.conveyal.gtfs;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 public class GTFSMainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void canPrintHelp() throws Exception {
+        String[] args = {"-help"};
+        GTFSMain.main(args);
+        assertThat(outContent.toString(), containsString("usage: java"));
+    }
+
+    @Test
+    public void exitsIfNoArgumentsProvided() throws Exception {
+        exit.expectSystemExitWithStatus(1);
+        String[] args = {};
+        GTFSMain.main(args);
+        assertThat(outContent.toString(), containsString("usage: java"));
+    }
+
+    @Test
+    public void canValidateSimpleAgency() throws Exception {
+        String[] args = {"./src/test/resources/fake-agency.zip", "-validate"};
+        GTFSMain.main(args);
+    }
 }
