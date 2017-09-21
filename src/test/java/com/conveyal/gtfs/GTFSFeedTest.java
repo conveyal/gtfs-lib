9678846dd95e06657a9cde371b86b3dfa271741e
Evan Siroky <evan.siroky@yahoo.com>
2017-09-21 11:41:54 -0700

    refactor and correct the GTFSFeedTest

--- src/test/java/com/conveyal/gtfs/GTFSFeedTest.java
+++ src/test/java/com/conveyal/gtfs/GTFSFeedTest.java
@@ -14,7 +14,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import static com.conveyal.gtfs.TestUtils.getResourceFileName;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,110 +25,131 @@ public class GTFSFeedTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(GTFSFeedTest.class);
 
+    private static class FileTestCase {
+        public String filename;
+        public DataExpectation[] expectedColumnData;
+
+        public FileTestCase(String filename, DataExpectation[] expectedColumnData) {
+            this.filename = filename;
+            this.expectedColumnData = expectedColumnData;
+        }
+    }
+
+    private static class DataExpectation {
+        public String columnName;
+        public String expectedValue;
+
+        public DataExpectation(String columnName, String expectedValue) {
+            this.columnName = columnName;
+            this.expectedValue = expectedValue;
+        }
+    }
+
     /**
      * Make sure a roundtrip of loading a GTFS zip file and then writing another zip file can be performed.
      */
     @Test
     public void canDoRoundtripLoadAndWriteToZipFile() throws IOException {
-        String outZip = getResourceFileName("fake-agency-output.zip");
-        File file = new File(outZip);
-        file.delete();
-        try {
-            GTFSFeed feed = new GTFSFeed();
-            feed.fromFile(getResourceFileName("fake-agency.zip"));
-            feed.toFile(outZip);
-            assertThat(file.exists(), equalTo(true));
+        // create a temp file for this test
+        File outZip = File.createTempFile("fake-agency-output", ".zip");
 
-            // assert that rows of data were written to files within the zipfile
-            ZipFile zip = new ZipFile(file);
+        // delete file to make sure we can assert that this program created the file
+        outZip.delete();
 
-            FileTestCase[] fileTestCases = {
-                // agency.txt
-                new FileTestCase(
-                    "agency.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("agency_id", "1"),
-                        new DataExpectation("agency_name", "Fake Transit")
-                    }
-                ),
-                new FileTestCase(
-                    "calendar.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("service_id", "04100312-8fe1-46a5-a9f2-556f39478f57"),
-                        new DataExpectation("start_date", "20170915"),
-                        new DataExpectation("end_date", "20170917")
-                    }
-                ),
-                new FileTestCase(
-                    "routes.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("agency_id", "1"),
-                        new DataExpectation("route_id", "1"),
-                        new DataExpectation("route_long_name", "Route 1")
-                    }
-                ),
-                new FileTestCase(
-                    "shapes.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("shape_id", "5820f377-f947-4728-ac29-ac0102cbc34e"),
-                        new DataExpectation("shape_pt_lat", "37.0612132"),
-                        new DataExpectation("shape_pt_lon", "-122.0074332")
-                    }
-                ),
-                new FileTestCase(
-                    "stop_times.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("trip_id", "a30277f8-e50a-4a85-9141-b1e0da9d429d"),
-                        new DataExpectation("departure_time", "07:00:00"),
-                        new DataExpectation("stop_id", "4u6g")
-                    }
-                ),
-                new FileTestCase(
-                    "trips.txt",
-                    new DataExpectation[]{
-                        new DataExpectation("route_id", "1"),
-                        new DataExpectation("trip_id", "a30277f8-e50a-4a85-9141-b1e0da9d429d"),
-                        new DataExpectation("service_id", "04100312-8fe1-46a5-a9f2-556f39478f57")
-                    }
-                )
-            };
+        GTFSFeed feed = GTFSFeed.fromFile(getResourceFileName("fake-agency.zip"));
+        feed.toFile(outZip.getAbsolutePath());
+        feed.close();
+        assertThat(outZip.exists(), is(true));
 
+        // assert that rows of data were written to files within the zipfile
+        ZipFile zip = new ZipFile(outZip);
 
-            for (FileTestCase fileTestCase: fileTestCases) {
-                ZipEntry entry = zip.getEntry(fileTestCase.filename);
-                assertThat(entry, notNullValue());
-                InputStream zis = zip.getInputStream(entry);
-                InputStream bis = new BOMInputStream(zis);
-                CsvReader reader = new CsvReader(bis, ',', Charset.forName("UTF8"));
-                boolean hasHeaders = reader.readHeaders();
-                assertThat(hasHeaders, is(true));
-                reader.readRecord();
-                for (DataExpectation dataExpectation: fileTestCase.expectedColumnData) {
-                    assertThat(reader.get(dataExpectation.columnName), equalTo(dataExpectation.expectedValue));
+        FileTestCase[] fileTestCases = {
+            // agency.txt
+            new FileTestCase(
+                "agency.txt",
+                new DataExpectation[]{
+                    new DataExpectation("agency_id", "1"),
+                    new DataExpectation("agency_name", "Fake Transit")
+                }
+            ),
+            new FileTestCase(
+                "calendar.txt",
+                new DataExpectation[]{
+                    new DataExpectation("service_id", "04100312-8fe1-46a5-a9f2-556f39478f57"),
+                    new DataExpectation("start_date", "20170915"),
+                    new DataExpectation("end_date", "20170917")
+                }
+            ),
+            new FileTestCase(
+                "routes.txt",
+                new DataExpectation[]{
+                    new DataExpectation("agency_id", "1"),
+                    new DataExpectation("route_id", "1"),
+                    new DataExpectation("route_long_name", "Route 1")
+                }
+            ),
+            new FileTestCase(
+                "shapes.txt",
+                new DataExpectation[]{
+                    new DataExpectation("shape_id", "5820f377-f947-4728-ac29-ac0102cbc34e"),
+                    new DataExpectation("shape_pt_lat", "37.0612132"),
+                    new DataExpectation("shape_pt_lon", "-122.0074332")
+                }
+            ),
+            new FileTestCase(
+                "stop_times.txt",
+                new DataExpectation[]{
+                    new DataExpectation("trip_id", "a30277f8-e50a-4a85-9141-b1e0da9d429d"),
+                    new DataExpectation("departure_time", "07:00:00"),
+                    new DataExpectation("stop_id", "4u6g")
+                }
+            ),
+            new FileTestCase(
+                "trips.txt",
+                new DataExpectation[]{
+                    new DataExpectation("route_id", "1"),
+                    new DataExpectation("trip_id", "a30277f8-e50a-4a85-9141-b1e0da9d429d"),
+                    new DataExpectation("service_id", "04100312-8fe1-46a5-a9f2-556f39478f57")
+                }
+            )
+        };
+
+        // look through all written files in the zipfile
+        for (FileTestCase fileTestCase: fileTestCases) {
+            ZipEntry entry = zip.getEntry(fileTestCase.filename);
+
+            // make sure the file exists within the zipfile
+            assertThat(entry, notNullValue());
+
+            // create csv reader for file
+            InputStream zis = zip.getInputStream(entry);
+            InputStream bis = new BOMInputStream(zis);
+            CsvReader reader = new CsvReader(bis, ',', Charset.forName("UTF8"));
+
+            // make sure the file has headers
+            boolean hasHeaders = reader.readHeaders();
+            assertThat(hasHeaders, is(true));
+
+            // make sure that the a record matching the expected row exists in this table
+            boolean recordFound = false;
+            while (reader.readRecord() && !recordFound) {
+                boolean allExpectationsMetForThisRecord = true;
+                for (DataExpectation dataExpectation : fileTestCase.expectedColumnData) {
+                    if(!reader.get(dataExpectation.columnName).equals(dataExpectation.expectedValue)) {
+                        allExpectationsMetForThisRecord = false;
+                        break;
+                    }
+                }
+                if (allExpectationsMetForThisRecord) {
+                    recordFound = true;
                 }
             }
-        } finally {
-            file.delete();
+            assertThat(
+                "Data Expectation record not found in " + fileTestCase.filename,
+                recordFound,
+                is(true)
+            );
         }
-    }
-}
-
-class FileTestCase {
-    public String filename;
-    public DataExpectation[] expectedColumnData;
-
-    public FileTestCase(String filename, DataExpectation[] expectedColumnData) {
-        this.filename = filename;
-        this.expectedColumnData = expectedColumnData;
-    }
-}
-
-class DataExpectation {
-    public String columnName;
-    public String expectedValue;
-
-    public DataExpectation(String columnName, String expectedValue) {
-        this.columnName = columnName;
-        this.expectedValue = expectedValue;
     }
 }
