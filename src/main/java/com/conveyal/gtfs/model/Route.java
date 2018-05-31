4accbf2d0a46ff2581df823a5a3dc8f246cfb409
Landon Reed <landontreed@gmail.com>
2018-05-31 08:21:35 -0400

    add set statement parameter for wheelchair_accessible editor field

--- src/main/java/com/conveyal/gtfs/model/Route.java
+++ src/main/java/com/conveyal/gtfs/model/Route.java
@@ -56,7 +56,8 @@ public class Route extends Entity { // implements Entity.Factory<Route>
         statement.setString(oneBasedIndex++, route_url != null ? route_url.toString() : null);
         statement.setString(oneBasedIndex++, route_color);
         statement.setString(oneBasedIndex++, route_text_color);
-        // Editor-specific fields publicly_visible and status.
+        // Editor-specific fields publicly_visible, wheelchair_accessible, and status.
+        setIntParameter(statement, oneBasedIndex++, 0);
         setIntParameter(statement, oneBasedIndex++, 0);
         setIntParameter(statement, oneBasedIndex++, 0);
     }
