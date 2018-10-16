305df47c4cfd4492b3694e3a9027ceba26968e4b
Landon Reed <landontreed@gmail.com>
2018-10-16 09:53:58 -0400

    test(editor): add basic CRUD test for fares

--- src/test/java/com/conveyal/gtfs/loader/JDBCTableWriterTest.java
+++ src/test/java/com/conveyal/gtfs/loader/JDBCTableWriterTest.java
@@ -1,6 +1,7 @@
 package com.conveyal.gtfs.loader;
 
 import com.conveyal.gtfs.TestUtils;
+import com.conveyal.gtfs.util.FareDTO;
 import com.conveyal.gtfs.util.FeedInfoDTO;
 import com.conveyal.gtfs.util.InvalidNamespaceException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,6 +29,11 @@ public class JDBCTableWriterTest {
     private static String testDBName;
     private static DataSource testDataSource;
     private static String testNamespace;
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private static JdbcTableWriter createTestTableWriter (Table table) throws InvalidNamespaceException {
+        return new JdbcTableWriter(table, testDataSource, testNamespace);
+    }
 
     @BeforeClass
     public static void setUpClass() throws SQLException {
@@ -35,7 +41,7 @@ public class JDBCTableWriterTest {
         testDBName = TestUtils.generateNewDB();
         String dbConnectionUrl = String.format("jdbc:postgresql://localhost/%s", testDBName);
         testDataSource = createDataSource (dbConnectionUrl, null, null);
-        LOG.info("creating feeds table because it isn't automtically generated unless you import a feed");
+        LOG.info("creating feeds table because it isn't automatically generated unless you import a feed");
         Connection connection = testDataSource.getConnection();
         connection.createStatement()
             .execute("create table if not exists feeds (namespace varchar primary key, md5 varchar, " +
@@ -50,8 +56,10 @@ public class JDBCTableWriterTest {
     }
 
     @Test
-    public void canCreateUpdateAndDeleteFeedinfoEntities () throws IOException, SQLException, InvalidNamespaceException {
-        ObjectMapper mapper = new ObjectMapper();
+    public void canCreateUpdateAndDeleteFeedInfoEntities() throws IOException, SQLException, InvalidNamespaceException {
+        // Store Table and Class values for use in test.
+        final Table feedInfoTable = Table.FEED_INFO;
+        final Class<FeedInfoDTO> feedInfoDTOClass = FeedInfoDTO.class;
 
         // create new object to be saved
         FeedInfoDTO feedInfoInput = new FeedInfoDTO();
@@ -63,13 +71,13 @@ public class JDBCTableWriterTest {
         feedInfoInput.default_route_type = "3";
 
         // convert object to json and save it
-        JdbcTableWriter createTableWriter = new JdbcTableWriter(Table.FEED_INFO, testDataSource, testNamespace);
+        JdbcTableWriter createTableWriter = createTestTableWriter(feedInfoTable);
         String createOutput = createTableWriter.create(mapper.writeValueAsString(feedInfoInput), true);
         LOG.info("create output:");
         LOG.info(createOutput);
 
         // parse output
-        FeedInfoDTO createdFeedInfo = mapper.readValue(createOutput, FeedInfoDTO.class);
+        FeedInfoDTO createdFeedInfo = mapper.readValue(createOutput, feedInfoDTOClass);
 
         // make sure saved data matches expected data
         assertThat(createdFeedInfo.feed_publisher_name, equalTo(publisherName));
@@ -79,7 +87,7 @@ public class JDBCTableWriterTest {
         createdFeedInfo.feed_publisher_name = updatedPublisherName;
 
         // covert object to json and save it
-        JdbcTableWriter updateTableWriter = new JdbcTableWriter(Table.FEED_INFO, testDataSource, testNamespace);
+        JdbcTableWriter updateTableWriter = createTestTableWriter(feedInfoTable);
         String updateOutput = updateTableWriter.update(
             createdFeedInfo.id,
             mapper.writeValueAsString(createdFeedInfo),
@@ -88,13 +96,13 @@ public class JDBCTableWriterTest {
         LOG.info("update output:");
         LOG.info(updateOutput);
 
-        FeedInfoDTO updatedFeedInfoDTO = mapper.readValue(updateOutput, FeedInfoDTO.class);
+        FeedInfoDTO updatedFeedInfoDTO = mapper.readValue(updateOutput, feedInfoDTOClass);
 
         // make sure saved data matches expected data
         assertThat(updatedFeedInfoDTO.feed_publisher_name, equalTo(updatedPublisherName));
 
         // try to delete record
-        JdbcTableWriter deleteTableWriter = new JdbcTableWriter(Table.FEED_INFO, testDataSource, testNamespace);
+        JdbcTableWriter deleteTableWriter = createTestTableWriter(feedInfoTable);
         int deleteOutput = deleteTableWriter.delete(
             createdFeedInfo.id,
             true
@@ -106,7 +114,7 @@ public class JDBCTableWriterTest {
         String sql = String.format(
             "select * from %s.%s where id=%d",
             testNamespace,
-            Table.FEED_INFO.name,
+            feedInfoTable.name,
             createdFeedInfo.id
         );
         LOG.info(sql);
@@ -116,8 +124,6 @@ public class JDBCTableWriterTest {
 
     @Test
     public void canPreventSQLInjection() throws IOException, SQLException, InvalidNamespaceException {
-        ObjectMapper mapper = new ObjectMapper();
-
         // create new object to be saved
         FeedInfoDTO feedInfoInput = new FeedInfoDTO();
         String publisherName = "' OR 1 = 1; SELECT '1";
@@ -128,7 +134,7 @@ public class JDBCTableWriterTest {
         feedInfoInput.default_route_type = "3";
 
         // convert object to json and save it
-        JdbcTableWriter createTableWriter = new JdbcTableWriter(Table.FEED_INFO, testDataSource, testNamespace);
+        JdbcTableWriter createTableWriter = createTestTableWriter(Table.FEED_INFO);
         String createOutput = createTableWriter.create(mapper.writeValueAsString(feedInfoInput), true);
         LOG.info("create output:");
         LOG.info(createOutput);
@@ -138,6 +144,75 @@ public class JDBCTableWriterTest {
 
         // make sure saved data matches expected data
         assertThat(createdFeedInfo.feed_publisher_name, equalTo(publisherName));
+    }
+
+    @Test
+    public void canCreateUpdateAndDeleteFares() throws IOException, SQLException, InvalidNamespaceException {
+        // Store Table and Class values for use in test.
+        final Table fareTable = Table.FARE_ATTRIBUTES;
+        final Class<FareDTO> fareDTOClass = FareDTO.class;
+
+        // create new object to be saved
+        FareDTO fareInput = new FareDTO();
+        String fareId = "2A";
+        fareInput.fare_id = fareId;
+        fareInput.currency_type = "USD";
+        fareInput.price = 2.50;
+        fareInput.agency_id = "RTA";
+        fareInput.payment_method = 0;
+        fareInput.transfers = null;
+        fareInput.fare_rules = new FareDTO.FareRuleDTO[]{};
+
+        // convert object to json and save it
+        JdbcTableWriter createTableWriter = createTestTableWriter(fareTable);
+        String createOutput = createTableWriter.create(mapper.writeValueAsString(fareInput), true);
+        LOG.info("create output:");
+        LOG.info(createOutput);
+
+        // parse output
+        FareDTO createdFare = mapper.readValue(createOutput, fareDTOClass);
+
+        // make sure saved data matches expected data
+        assertThat(createdFare.fare_id, equalTo(fareId));
+
+        // try to update record
+        String updatedFareId = "3B";
+        createdFare.fare_id = updatedFareId;
+
+        // covert object to json and save it
+        JdbcTableWriter updateTableWriter = createTestTableWriter(fareTable);
+        String updateOutput = updateTableWriter.update(
+                createdFare.id,
+                mapper.writeValueAsString(createdFare),
+                true
+        );
+        LOG.info("update output:");
+        LOG.info(updateOutput);
+
+        FareDTO updatedFareDTO = mapper.readValue(updateOutput, fareDTOClass);
+
+        // make sure saved data matches expected data
+        assertThat(updatedFareDTO.fare_id, equalTo(updatedFareId));
+
+        // try to delete record
+        JdbcTableWriter deleteTableWriter = createTestTableWriter(fareTable);
+        int deleteOutput = deleteTableWriter.delete(
+                createdFare.id,
+                true
+        );
+        LOG.info("delete output:");
+        LOG.info(updateOutput);
+
+        // make sure record does not exist in DB
+        String sql = String.format(
+                "select * from %s.%s where id=%d",
+                testNamespace,
+                fareTable.name,
+                createdFare.id
+        );
+        LOG.info(sql);
+        ResultSet rs = testDataSource.getConnection().prepareStatement(sql).executeQuery();
+        assertThat(rs.getFetchSize(), equalTo(0));
     }
 
     @AfterClass
