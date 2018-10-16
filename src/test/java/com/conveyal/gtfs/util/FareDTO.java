a6943aa485f0069428e7593cb222c826ce46c904
Landon Reed <landontreed@gmail.com>
2018-10-16 09:57:39 -0400

    test: add FareDTO class

---
+++ src/test/java/com/conveyal/gtfs/util/FareDTO.java
@@ -0,0 +1,29 @@
+package com.conveyal.gtfs.util;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * DTO used to model expected fare JSON structure. NOTE: reference types (e.g., Integer and Double) are used here in
+ * order to model null/empty values in JSON object.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FareDTO {
+    public int id;
+    public String fare_id;
+    public Double price;
+    public String currency_type;
+    public Integer payment_method;
+    public Integer transfers;
+    public String agency_id;
+    public Double transfer_duration;
+    public FareRuleDTO[] fare_rules;
+
+    public class FareRuleDTO {
+        public int id;
+        public String fare_id;
+        public String route_id;
+        public String contains_id;
+        public String origin_id;
+        public String destination_id;
+    }
+}
