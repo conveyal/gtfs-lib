df8d4ebdaf8380a31df957c0ebf6f9bfcb7717e8
evansiroky <evan.siroky@yahoo.com>
2019-03-27 10:25:07 -0700

    refactor(test): reformat informational file in resource dir to be in .md format

--- src/test/resources/real-world-gtfs-feeds/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains real-life GTFS feeds. While it's best to reduce data to the simplest form so problems can be
- * isolated more easily, adding these feeds can be very useful for creating test cases that prove bugs exist in certain
- * edge cases.
- */
-
-package com.conveyal.gtfs.dto;
-
