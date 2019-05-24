2847fb3118fe24c398cad02756f83e23a32c0a4a
Landon Reed <landontreed@gmail.com>
2019-05-24 12:07:30 -0400

    refactor(validator): add javadoc to ErrorExpectations

--- src/test/java/com/conveyal/gtfs/storage/ErrorExpectation.java
+++ src/test/java/com/conveyal/gtfs/storage/ErrorExpectation.java
@@ -2,6 +2,14 @@ package com.conveyal.gtfs.storage;
 
 import com.conveyal.gtfs.error.NewGTFSErrorType;
 
+/**
+ * Defines the expected values for an error stored in the errors table for a feed in the GTFS database.
+ *
+ * Note: the errors should be listed in order that they are expected to be encountered. Check out
+ * {@link com.conveyal.gtfs.loader.JdbcGtfsLoader#loadTables} to see the order in which tables are loaded,
+ * {@link com.conveyal.gtfs.loader.Feed#validate()} to see the order in which validators are called (trip validator
+ * order can be found in {@link com.conveyal.gtfs.validator.NewTripTimesValidator}).
+ */
 public class ErrorExpectation {
     public NewGTFSErrorType errorType;
     public String badValue;
@@ -24,6 +32,9 @@ public class ErrorExpectation {
         this.entityId = entityId;
     }
 
+    /**
+     * Constructs an array of error expectations from the input args.
+     */
     public static ErrorExpectation[] list (ErrorExpectation... expectations) {
         return expectations;
     }
